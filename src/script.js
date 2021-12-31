ac7e65849d0e11f4daf16c6c9e89f9c0acb0a6ea
smileyuformba21 <95450389+smileyuformba21@users.noreply.github.com>
2021-12-31 16:46:59 +0000

    updated JS with date and time

--- src/script.js
+++ src/script.js
@@ -1,6 +1,27 @@
-let now = new Date();
-let hours = now.getHours;
-console.log(hours);
+function currentDate(Date) {
+  let now = new Date();
+  let hours = now.getHours;
+  console.log(hours);
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes;
+  if (minutes > 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+currentDate(Date);
 function showTemperature(response) {
   console.log(response.data);
   let tempElement = document.querySelector("#current-temp");
